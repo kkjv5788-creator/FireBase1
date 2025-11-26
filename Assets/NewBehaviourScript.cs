e9129915164e27e4dd7297a267d028aacb630b23
kkjv5788 <kkjv5788@gmail.com>
2025-11-26 22:20:29 +0900

    수정

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -7,7 +8,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("gd");
     }
 
     // Update is called once per frame
